68d15565554396a0ca79cb502a80d4c7158a0e69
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-06-25 09:15:40 -0600

    testing

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
-import sdk from 'react-native-zksync-sso';
-import { type RpId } from 'react-native-zksync-sso';
 import LoggedOutView from '@/components/LoggedOutView';
 import { AccountDetails } from '@/types';
+
+import sdk from "react-native-zksync-sso";
+import { type RpId } from 'react-native-zksync-sso';
+const randomChallenge = sdk.utils.generateRandomChallenge();
+console.log('randomChallenge: ', randomChallenge);
 
 interface LoginProps {
   rpId: RpId;
@@ -15,13 +18,14 @@ const Login: React.FC<LoginProps> = () => {
     null,
   );
 
-  // Initialize platform-specific logging before any SDK usage
-  sdk.utils.initializePlatformLogger("io.jackpooley.MLSSOExampleRN");
+  // // Initialize platform-specific logging before any SDK usage
+  // sdk.utils.initializePlatformLogger("io.jackpooley.MLSSOExampleRN");
 
-  const rpId = sdk.utils.createRpId(
-    "soo-sdk-example-pages.pages.dev", // RP ID (same for both platforms)
-    "android:apk-key-hash:..." // Android origin - ignored on iOS
-  );
+  // const rpId = sdk.utils.createRpId(
+  //   "soo-sdk-example-pages.pages.dev", // RP ID (same for both platforms)
+  //   "android:apk-key-hash:..." // Android origin - ignored on iOS
+  // );
+  const rpId: any = '123';
  
   console.log("ACCOUNT DETAILS: ", accountDetails);
 
